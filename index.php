936fecf2a9ea2136d5c9217ab2161a6d9fac685a
TrojanB <qwertroya@gmail.com>
2023-03-08 09:35:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hurtowania papiernicza</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+
+    <div id="header">
+        <h1>W naszej hurtowni kupisz najtaniej</h1>
+    </div>
+
+    <div id="lewy">
+        <h3>Ceny wybranych artykułów w hurtowni</h3>
+        
+    </div>
+
+    <div id="srodkowy">
+
+    </div>
+
+    <div id="prawy">
+
+    </div>
+
+    <footer></footer>
+
+</body>
+</html>
